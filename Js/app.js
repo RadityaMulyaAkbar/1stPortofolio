e74edcf317249b11b0f250e9cca6104235595fae
Radit <raditss674@gmail.com>
2022-12-20 21:55:09 +0800

    improve fot a responsive

--- Js/app.js
+++ Js/app.js
@@ -18,6 +18,8 @@ const links = document.querySelectorAll(".nav-link");
 
 const toggle_btn = document.querySelector(".toggle-btn");
 
+const hamburger = document.querySelector(".hamburger");
+
 window.addEventListener("scroll", () => {
   activeLink();
   if (!skillsPlayed) skillsCounter();
@@ -211,3 +213,15 @@ toggle_btn.addEventListener("click", () => {
 });
 
 /* --------------- Open & Close Navbar Menu --------------- */
+
+hamburger.addEventListener("click", () => {
+  document.body.classList.toggle("open");
+  document.body.classList.toggle("stopScrolling");
+});
+
+links.forEach((link) =>
+   link.addEventListener("click", () => {
+    document.body.classList.remove("open");
+    document.body.classList.remove("stopScrolling");
+  })
+);
